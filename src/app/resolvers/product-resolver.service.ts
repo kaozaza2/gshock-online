10dd492183456c40e0e65656da831ac0664c3243
Moki <kittisakphiri@gmail.com>
2021-03-11 16:20:20 +0700

    add default product-resolver logic

--- src/app/resolvers/product-resolver.service.ts
+++ src/app/resolvers/product-resolver.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ProductService } from '../services/product.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductResolverService {
+export class ProductResolverService implements Resolve<any> {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private productService: ProductService
+  ) {
+    // Empty constructor
+  }
+
+  resolve(route: ActivatedRouteSnapshot) {
+    const productId = Number(route.paramMap.get('productId'));
+    return this.productService.getProduct(productId);
+  }
 }
